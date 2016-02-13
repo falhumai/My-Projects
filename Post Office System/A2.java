4d386de65bb1cfafcb228695b967ce078e533e5d
Faisal Al-Humaimidi <falhumai96@gmail.com>
2016-02-13 12:03:10 -0800

    (version 0.1)

--- Post Office System/A2.java
+++ Post Office System/A2.java
@@ -1,16 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.io.*;
 import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.math.BigDecimal;
 
-/**
- *
- * @author falhumai
- */
 public class A2 {
 
     public static boolean dayEntry(String file, int day) {
